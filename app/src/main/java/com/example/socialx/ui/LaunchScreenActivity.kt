c0d3427c8c8d41b92082040b1f0a038a6b172b0a
arpitkatiyar1999 <arpitkatiyar2018@gmail.com>
2022-09-09 10:15:11 +0530

    Minor Project Updates

--- app/src/main/java/com/example/socialx/ui/LaunchScreenActivity.kt
+++ app/src/main/java/com/example/socialx/ui/LaunchScreenActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.socialx.ui.login.MainActivity
+import com.example.socialx.ui.news.NewsDetailActivity
 
 class LaunchScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/socialx/ui/news/NewsDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.socialx.ui.news
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.socialx.R
+
+class NewsDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_detail)
+    }
+}
